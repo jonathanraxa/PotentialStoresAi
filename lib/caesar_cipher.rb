d8201a35cd1bb4a8cf0eed709efff36f198d0607
Bradley Showen <BShowen@me.com>
2019-06-27 19:38:31 -0400

    refactor loops to functions

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,19 +1,22 @@
-def caesar_cipher string, shift
+def caesar_cipher(string, shift)
     letters = string.split('')
     letters.map! do |letter|
         number = letter.ord
-        number += shift if letter.between?('a','z') || letter.between?('A', 'Z')
+        # number += shift if letter.between?('a','z') || letter.between?('A', 'Z')
         if (97..122).include? letter.ord
-            until number <= 122 #this loop will make sure your letter stays inside the alphabet. 
-                number = (number % 122) + 96
-            end
+            return shift_97(shift).chr
         elsif (65..90).include? letter.ord
-            until number <= 90 #this loop will make sure your letter stays inside the alphabet. 
-                number = (number % 90) + 64
-            end
+            return shift_65(shift).chr
         end
-        number.chr
+        # number.chr
     end
     
     return letters.join('')
+end
+
+def shift_97(number)
+    (number % 26) + 97
+end 
+def shift_65(number)
+    (number % 26) + 65
 end
