39b350ef31fe1eeb8f1ecca76c41d4d9e5117524
Bradley Showen <BShowen@me.com>
2019-06-29 13:48:51 -0400

    rename params for better reading

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -4,30 +4,30 @@ class CaesarCipher
         @letters = word.split("")
     end
 
-    def shift(shift)
+    def shift(shift_factor)
         @letters.map! do |letter|
             position_in_alpabet = letter.ord
             if (97..122).include? letter.ord #if the letter is lower case
-                case shift >= 0 #check if posotive or negative shift
+                case shift_factor >= 0 #check if posotive or negative shift
                 when true
-                    lowercase_shift(position_in_alpabet, shift).chr
+                    lowercase_shift(position_in_alpabet, shift_factor).chr
                 when false
-                    lowercase_backshift(position_in_alpabet, shift).chr
+                    lowercase_backshift(position_in_alpabet, shift_factor).chr
                 end
             elsif (65..90).include? letter.ord #if the letter is upper case
-                case shift >= 0 # check if posotive or negative shift
+                case shift_factor >= 0 # check if posotive or negative shift
                 when true
-                    uppercase_shift(position_in_alpabet,shift).chr
+                    uppercase_shift(position_in_alpabet,shift_factor).chr
                 when false
-                    uppercase_backshift(position_in_alpabet, shift).chr
+                    uppercase_backshift(position_in_alpabet, shift_factor).chr
                 end
             end
         end
         @letters.join('')
     end
     
-    def uppercase_shift(current_pos, shift)
-        new_position = current_pos + (shift % 26)
+    def uppercase_shift(current_pos, shift_factor)
+        new_position = current_pos + (shift_factor % 26)
         if new_position > 90
             (new_position % 90) + 64
         else
@@ -35,12 +35,12 @@ class CaesarCipher
         end
     end
     
-    def uppercase_backshift(number, shift)
-        (number + shift) > 65 ? (number + shift) : uppercase_shift(number, (26 - shift.abs))
+    def uppercase_backshift(number, shift_factor)
+        (number + shift_factor) > 65 ? (number + shift_factor) : uppercase_shift(number, (26 - shift_factor.abs))
     end
     
-    def lowercase_shift(current_pos, shift) 
-        new_position = current_pos + (shift % 26)
+    def lowercase_shift(current_pos, shift_factor) 
+        new_position = current_pos + (shift_factor % 26)
         if new_position > 122
             (new_position % 122) + 96
         else
@@ -48,7 +48,7 @@ class CaesarCipher
         end
     end
     
-    def lowercase_backshift(number, shift)
-        (number + shift) > 97 ? (number + shift) : lowercase_shift(number, (26 - shift.abs)) 
+    def lowercase_backshift(number, shift_factor)
+        (number + shift_factor) > 97 ? (number + shift_factor) : lowercase_shift(number, (26 - shift_factor.abs)) 
     end
 end
